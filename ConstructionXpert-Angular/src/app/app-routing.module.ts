e96f68eb81673e58c747a8e5140d7ffefee30462
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-12 12:27:57 +0100

    update

--- ConstructionXpert-Angular/src/app/app-routing.module.ts
+++ ConstructionXpert-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./components/auth/login/login.component";
+import {RegisterComponent} from "./components/auth/register/register.component";
+import {AddRessourcesComponent} from "./components/Ressources/add-ressources/add-ressources.component";
+import {EditRessourcesComponent} from "./components/Ressources/edit-ressources/edit-ressources.component";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'add-ressource',component:AddRessourcesComponent},
+  {path:'edit/:id', component:EditRessourcesComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
