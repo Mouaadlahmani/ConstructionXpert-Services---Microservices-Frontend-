922dd71c6acde4fe1382e3d6b04c03b24f732d73
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-12 23:05:53 +0100

    Ajouter navbar et logout

--- ConstructionXpert-Angular/src/app/app.component.ts
+++ ConstructionXpert-Angular/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ConstructionXpert-Angular';
+  protected showNavButtons: boolean=false;
+
+
+  constructor(private service: AuthService,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.checkpath();
+    this.router.events.subscribe(() => this.checkpath());
+  }
+
+  protected checkpath() {
+    const url = this.router.url;
+    this.showNavButtons = url.startsWith('/login');
+  }
+
+  logout():void{
+    this.service.logout();
+    this.router.navigate(['login']);
+  }
 }
