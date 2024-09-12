7ba82ab6b5104dc429763c49104e6bb37e9a0e94
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-12 12:18:02 +0100

    Creation de class Utilisateur

--- ConstructionXpert-Angular/src/app/app.module.ts
+++ ConstructionXpert-Angular/src/app/app.module.ts
@@ -4,15 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { RessourcesComponent } from './components/Ressources/ressources/ressources.component';
+import { AddRessourcesComponent } from './components/Ressources/add-ressources/add-ressources.component';
+import { EditRessourcesComponent } from './components/Ressources/edit-ressources/edit-ressources.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    RessourcesComponent,
+    AddRessourcesComponent,
+    EditRessourcesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ConstructionXpert-Angular/src/app/classes/Utilisateur/utilisateur.ts
@@ -0,0 +1,6 @@
+export class Utilisateur {
+  id!:number;
+  username!:string;
+  email!:string;
+  password!:string;
+}
