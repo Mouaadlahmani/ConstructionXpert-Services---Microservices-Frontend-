8eead300b0d2ce87fc370234c619835ea0580f8f
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-14 23:58:26 +0100

    update

--- ConstructionXpert-Angular/src/app/app.module.ts
+++ ConstructionXpert-Angular/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { RouterModule } from '@angular/router';  // Importez RouterModule
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
--- ConstructionXpert-Angular/src/app/services/projet/projet.service.ts
+++ ConstructionXpert-Angular/src/app/services/projet/projet.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Projets } from '../model/projet';
+import {Projets} from "../../model/projet";
 
 @Injectable({
   providedIn: 'root'
